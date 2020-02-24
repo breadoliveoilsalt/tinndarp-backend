ed42fd796cf7b2e9800b80c7cc9ca2fa8ca86485
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-24 12:30:10 -0500

    Update json to test deployment on PR merge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
   def main
     # render text: "got to home controller"
     render :json => { :status => "Made it to the HomeController",
-                      :new_status => "Successful deploy to Heroku"}
+                      :new_status => "Successful deploy to Heroku",
+                      :extra_status => "Confirm deploy on merging PR, for real this time"}
 
   end
 
