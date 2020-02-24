223bdfbca73735d56587110a5db1665f0d4d8710
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-24 12:21:58 -0500

    Add json to test if app deploys successfully to Heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@ class HomeController < ApplicationController
 
   def main
     # render text: "got to home controller"
-    render :json => {:status => "Made it to the HomeController"}
-    
+    render :json => { :status => "Made it to the HomeController",
+                      :new_status => "Successful deploy to Heroku"}
+
   end
 
 end
