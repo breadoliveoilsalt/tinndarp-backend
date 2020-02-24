d9401b7b8b7987906f2e10866c9b88fc8ad793b8
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-24 13:08:46 -0500

    Confirm travis runs and deploys on merging PR into master

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,10 @@
 class HomeController < ApplicationController
 
   def main
-    # render text: "got to home controller"
     render :json => { :status => "Made it to the HomeController",
                       :new_status => "Successful deploy to Heroku",
-                      :extra_status => "Confirm deploy on merging PR, for real this time"}
-
+                      :extra_status => "Confirm deploy on merging PR, for real this time"
+                    }
   end
 
 end
