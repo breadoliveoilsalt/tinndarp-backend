038a75617be2c5608c8199818a0c3b6dfdf6bfa6
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-25 14:20:02 -0500

    Add initial tests for ItemsController#index

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
 
+  describe "GET index" do
+    it "has a 200 status code" do
+      get :index
+
+      expect(response.status).to eq(200)
+    end
+
+    it "returns JSON" do
+      get :index
+
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+    end
+
+    describe "the rendered JSON" do
+      render_views
+
+      it "represents each of the Items" do
+        item_1 = Item.create!
+        item_2 = Item.create!
+
+        get :index
+
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response.length).to eq(2)
+      end
+    end
+  end
+
 end
