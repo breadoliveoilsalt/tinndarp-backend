27ebd66d6a9ee2100d2a948b121debb7d2d851fe
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-02 12:07:30 -0500

    Add failing tests for Item properties

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
+RSpec::Expectations.configuration.on_potential_false_positives = :nothing
+
 RSpec.describe Item, type: :model do
-  
+
+  before(:each) do
+    @item = Item.new
+  end
+
+  describe "the properties of an Item" do
+
+    it "an Item has an image_url" do
+      expect{@item.image_url}.to_not raise_error(NoMethodError)
+    end
+
+    it "an Item has a name" do
+      expect{@item.blah}.to_not raise_error(NoMethodError)
+    end
+
+    it "an Item has a price" do
+      expect{@item.price}.to_not raise_error(NoMethodError)
+    end
+
+    it "an Item has a description" do
+      expect{@item.description}.to_not raise_error(NoMethodError)
+    end
+  end
+
 end
