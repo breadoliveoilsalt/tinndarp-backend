42a64de91b61e2cc79b13cd954882b5bc961292e
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-05 14:09:32 -0500

    Add and pass test on valid password length

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,29 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "is not valid without an email" do
-    user = User.new(password: "password")
-    expect(user).to_not be_valid
-  end
+  describe "creating a User" do
 
-  it "is not valid without password" do
-    user = User.new(email: "email")
-    expect(user).to_not be_valid
-  end
+    valid_email = "tony@tony.com"
+    valid_password = "tonytony"
 
+    it "is not valid without an email" do
+      user = User.new(password: valid_password)
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid without password" do
+      user = User.new(email: valid_email)
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid with a password less than 8 characters" do
+      user_1 = User.new(email: valid_email, password: "1234567")
+      expect(user_1).to_not be_valid
+
+      user_2 = User.new(email: valid_email, password: "12345678")
+      expect(user_2).to be_valid
+    end
+
+  end
 
 end
