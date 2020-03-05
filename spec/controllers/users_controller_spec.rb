c6fb3a727d1347ed6061ee673a51ccef0cb59651
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-05 16:39:53 -0500

    Add failing test to UsersController for receiving error on invalid cred

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,9 +14,20 @@ RSpec.describe Api::UsersController, type: :controller do
       expect(response.content_type).to eq("application/json; charset=utf-8")
     end
 
-    xdescribe "the rendered JSON" do
+    describe "the rendered JSON" do
       render_views
 
+      it "renders an error if the user credentials are invalid" do
+        post :create, :params => {
+                        :user => {
+                          :email => "",
+                          :pasword => ""
+                        }
+                      }
+
+        parsed_response = json.parse(response.body)
+        expect(parsed_response.error).to be_truthy
+      end
       # it "represents each of the items" do
       #   item_1 = item.create!
       #   item_2 = item.create!
