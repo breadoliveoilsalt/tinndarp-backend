5806b584fe390b24a8fd1718adb3154ba8eb1558
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-05 13:55:42 -0500

    Add and pass test for User on needing password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-    it "is not valid without an email" do
-      user = User.new(password: "password")
-      expect(user).to_not be_valid
-    end
+  it "is not valid without an email" do
+    user = User.new(password: "password")
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without password" do
+    user = User.new(email: "email")
+    expect(user).to_not be_valid
+  end
 
 
 end
