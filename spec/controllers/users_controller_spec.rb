fdbde11f7d15cc0b2f5bbc479ea7b0a311f0d70d
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-06 10:09:03 -0500

    Refactor tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,24 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
 
-  def valid_params
-    {:params => {
-       :user => {
-          email: "billy@billy.com",
-          password: "billybilly"
-          }
+  before(:each) do
+    @valid_params =
+     { :params => {
+         :user => {
+            email: "billy@billy.com",
+            password: "billybilly"
+            }
+        }
       }
-    }
   end
 
   describe "POST create" do
     it "has a 200 status code" do
-      post :create, valid_params
+      post :create, @valid_params
       expect(response.status).to eq(200)
     end
 
     it "returns JSON" do
-      post :create, valid_params
+      post :create, @valid_params
 
       expect(response.content_type).to eq("application/json; charset=utf-8")
     end
@@ -56,26 +57,26 @@ RSpec.describe Api::UsersController, type: :controller do
           end
 
           it "raises an error if the params do not include a 'user' key" do
-            invalid_params = {:params => {
+            @invalid_params = {:params => {
               email: "billy@billy.com",
               password: "billybilly"
               }
             }
 
-            expect{post :create, invalid_params}.to raise_error(ActionController::ParameterMissing)
+            expect{post :create, @invalid_params}.to raise_error(ActionController::ParameterMissing)
           end
-
-      end
 
         context "signing in with valid credentials" do
 
           it "returns a key with signed_in set to true" do
-            post :create, valid_params
+            post :create, @valid_params
 
             parsed_response = JSON.parse(response.body)
 
             expect(parsed_response["logged_in"]).to eq("true")
           end
+
+        end
 
       end
 
