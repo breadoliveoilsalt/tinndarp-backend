1a3c5ffb6650797963fe5166b7b5ef4f094f6301
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-06 09:52:49 -0500

    Pass UsersController test on true logged_in

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,7 +7,7 @@ class Api::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render :json => {:controller => "user data"}
+      render :json => {:logged_in => "true"}
     else
       render :json => {:errors => user.errors.full_messages}
     end
