cbcd8deeba281fc66505aecda7d942e652cd385e
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-08 22:24:12 -0400

    Add skip verify_authenticity_token to UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def test
     render :json => {:controller => "You made it to the users controller!"}.to_json
