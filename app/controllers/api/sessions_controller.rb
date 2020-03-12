2e2f09f69fce2f71a0d6fee08ffe45f89ad55352
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-12 10:29:51 -0400

    Refactor to move repeated code to ApplicationController

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,11 +4,9 @@ class Api::SessionsController < ApplicationController
   def create
     user = User.find_by(email: user_params[:email])
     if user && user.authenticate(user_params[:password])
-      token_payload = {:user_id => user.id}
-
       render :json => {
         :logged_in => "true",
-        :token => encode(token_payload)
+        :token => renderTokenFor(user)
       }
     else
       render :json => {
@@ -17,36 +15,5 @@ class Api::SessionsController < ApplicationController
       }
    end
  end
-  #   user = User.new(user_params)
-  #   if user.save
-  #     token_payload = {:user_id => user.id}
-  #
-  #     render :json => {
-  #       :logged_in => "true",
-  #       :token => encode(token_payload)
-  #     }
-  #   else
-  #     render :json => {
-  #       :logged_in => "false",
-  #       :errors => user.errors.full_messages
-  #     }
-  #   end
-  # end
-
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
-
-  def encode(payload, expiration = 24.hours.from_now)
-    payload[:expiration] = expiration
-    JWT.encode payload, ENV["SECRET_KEY_BASE"], "HS256"
-  end
-
-  def decode(token)
-    JWT.decode token, ENV["SECRET_KEY_BASE"], true, { algorithm: "HS256" }
-  end
 
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,11 +4,9 @@ class Api::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      token_payload = {:user_id => user.id}
-
       render :json => {
         :logged_in => "true",
-        :token => encode(token_payload)
+        :token => renderTokenFor(user)
       }
     else
       render :json => {
@@ -16,22 +14,6 @@ class Api::UsersController < ApplicationController
         :errors => user.errors.full_messages
       }
     end
-  end
-
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
-
-  def encode(payload, expiration = 24.hours.from_now)
-    payload[:expiration] = expiration
-    JWT.encode payload, ENV["SECRET_KEY_BASE"], "HS256"
-  end
-
-  def decode(token)
-    JWT.decode token, ENV["SECRET_KEY_BASE"], true, { algorithm: "HS256" }
   end
 
 end
