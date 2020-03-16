676bbfd5defde07fd997fde7258f73cdc5ffc21a
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-16 10:51:54 -0400

    Add has_manys to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 8 }
+
+  has_many :liked_items, :through => :likes, :source => :item
+  has_many :noped_items, :through => :nopes, :source => :item
 end
