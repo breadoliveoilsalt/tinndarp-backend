a6644e1984c7d71f7de91e0adfa169f7914f2e31
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-16 11:29:54 -0400

    Add failing test on user liked_items

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe "creating a User" do
+  valid_email = "tony@tony.com"
+  valid_password = "tonytony"
 
-    valid_email = "tony@tony.com"
-    valid_password = "tonytony"
+  describe "creating a User" do
 
     it "is not valid without an email" do
       user = User.new(password: valid_password)
@@ -13,8 +13,8 @@ RSpec.describe User, type: :model do
     end
 
     it "requires a unique email address" do
-      user_1 = User.create(email: valid_email, password: valid_password)
-      expect(user_1).to be_valid
+      user_2 = User.create(email: valid_email, password: valid_password)
+      expect(user_2).to be_valid
 
       user_2 = User.create(email: valid_email, password: valid_password)
       expect(user_2).to_not be_valid
@@ -57,6 +57,41 @@ RSpec.describe User, type: :model do
       expect(user.password_digest).to be_truthy
       expect(user.password_digest).to_not equal(user.password)
     end
+  end
+
+  describe "liked_items" do
+
+    it "returns a list of liked items" do
+      item_1 = Item.create(
+        name: "SAGSTUA",
+        image_url: "https://www.ikea.com/us/en/images/products/sagstua-bed-frame__0783215_PE761511_S5.JPG?f=s",
+        price: "149.00",
+        description: "Bed frame, black, Full",
+        more_info_url: "https://www.ikea.com/us/en/p/sagstua-bed-frame-black-s59268898/"
+      )
+
+      item_2 = Item.create(
+        name: "MALM",
+        image_url: "https://www.ikea.com/us/en/images/products/malm-bed-frame-high__0637598_PE698416_S5.JPG?f=s",
+        price: "199.00",
+        description: "Bed frame, high, black-brown, Luröy, Queen",
+        more_info_url: "https://www.ikea.com/us/en/p/malm-bed-frame-high-black-brown-luroey-s69009475/"
+      )
+
+      item_3 = Item.create(
+        name: "BUSKBO",
+        image_url: "https://www.ikea.com/us/en/images/products/buskbo-armchair__0700959_PE723853_S5.JPG?f=s",
+        price: "130.00",
+        description: "Armchair, rattan",
+        more_info_url: "https://www.ikea.com/us/en/p/buskbo-armchair-rattan-70434311/"
+      )
+
+      user = User.create(email: valid_email, password: valid_password)
+      user.liked_items << [item_1, item_2, item_3]
+
+      expect(user.liked_items).to eq([item_1, item_2, item_3])
+    end
+
   end
 
 end
