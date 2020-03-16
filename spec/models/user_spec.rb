174f6eb02d05247af72df5081da9cd049e0c2601
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-16 11:35:07 -0400

    Pass user noped_items test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,9 +57,10 @@ RSpec.describe User, type: :model do
       expect(user.password_digest).to be_truthy
       expect(user.password_digest).to_not equal(user.password)
     end
+
   end
 
-  describe "liked_items" do
+  describe "#liked_items" do
 
     it "returns a list of liked items" do
       item_1 = Item.create(
@@ -90,6 +91,41 @@ RSpec.describe User, type: :model do
       user.liked_items << [item_1, item_2, item_3]
 
       expect(user.liked_items).to eq([item_1, item_2, item_3])
+    end
+
+  end
+
+  describe "#noped_items" do
+
+    it "returns a list of noped items" do
+      item_1 = Item.create(
+        name: "SAGSTUA",
+        image_url: "https://www.ikea.com/us/en/images/products/sagstua-bed-frame__0783215_PE761511_S5.JPG?f=s",
+        price: "149.00",
+        description: "Bed frame, black, Full",
+        more_info_url: "https://www.ikea.com/us/en/p/sagstua-bed-frame-black-s59268898/"
+      )
+
+      item_2 = Item.create(
+        name: "MALM",
+        image_url: "https://www.ikea.com/us/en/images/products/malm-bed-frame-high__0637598_PE698416_S5.JPG?f=s",
+        price: "199.00",
+        description: "Bed frame, high, black-brown, Luröy, Queen",
+        more_info_url: "https://www.ikea.com/us/en/p/malm-bed-frame-high-black-brown-luroey-s69009475/"
+      )
+
+      item_3 = Item.create(
+        name: "BUSKBO",
+        image_url: "https://www.ikea.com/us/en/images/products/buskbo-armchair__0700959_PE723853_S5.JPG?f=s",
+        price: "130.00",
+        description: "Armchair, rattan",
+        more_info_url: "https://www.ikea.com/us/en/p/buskbo-armchair-rattan-70434311/"
+      )
+
+      user = User.create(email: valid_email, password: valid_password)
+      user.noped_items << [item_1, item_2, item_3]
+
+      expect(user.noped_items).to eq([item_1, item_2, item_3])
     end
 
   end
