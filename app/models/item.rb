0d4e3878652b2df741a6c7a39f0a215105334b37
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-16 11:31:16 -0400

    Pass user like_items test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+
+  has_many :likes
   has_many :liking_users, :through => :likes, :source => :user
+  has_many :nopes
   has_many :noping_users, :through => :nopes, :source => :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 8 }
 
+  has_many :likes
   has_many :liked_items, :through => :likes, :source => :item
+  has_many :nopes
   has_many :noped_items, :through => :nopes, :source => :item
 end
