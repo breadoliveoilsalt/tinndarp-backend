49d4db1306020ec69fd3f8befa3a6d7a4cdb176c
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-16 11:59:06 -0400

    Pass items tests for nopes and noping_users

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -70,53 +70,44 @@ RSpec.describe Item, type: :model do
     end
 
   end
-  #
-  # describe "#nopes" do
-  #
-  #   it "returns a list of user nopes" do
-  #     user = User.create(email: valid_email, password: valid_password)
-  #     nope_1 = Nope.create(user_id: user.id, item_id: 1)
-  #     nope_2 = Nope.create(user_id: user.id, item_id: 2)
-  #     user.nopes << [nope_1, nope_2]
-  #
-  #     expect(user.nopes).to eq([nope_1, nope_2])
-  #   end
-  #
-  # end
-  #
-  # describe "#noped_items" do
-  #
-  #   it "returns a list of noped items" do
-  #     item_1 = Item.create(
-  #       name: "SAGSTUA",
-  #       image_url: "https://www.ikea.com/us/en/images/products/sagstua-bed-frame__0783215_PE761511_S5.JPG?f=s",
-  #       price: "149.00",
-  #       description: "Bed frame, black, Full",
-  #       more_info_url: "https://www.ikea.com/us/en/p/sagstua-bed-frame-black-s59268898/"
-  #     )
-  #
-  #     item_2 = Item.create(
-  #       name: "MALM",
-  #       image_url: "https://www.ikea.com/us/en/images/products/malm-bed-frame-high__0637598_PE698416_S5.JPG?f=s",
-  #       price: "199.00",
-  #       description: "Bed frame, high, black-brown, Luröy, Queen",
-  #       more_info_url: "https://www.ikea.com/us/en/p/malm-bed-frame-high-black-brown-luroey-s69009475/"
-  #     )
-  #
-  #     item_3 = Item.create(
-  #       name: "BUSKBO",
-  #       image_url: "https://www.ikea.com/us/en/images/products/buskbo-armchair__0700959_PE723853_S5.JPG?f=s",
-  #       price: "130.00",
-  #       description: "Armchair, rattan",
-  #       more_info_url: "https://www.ikea.com/us/en/p/buskbo-armchair-rattan-70434311/"
-  #     )
-  #
-  #     user = User.create(email: valid_email, password: valid_password)
-  #     user.noped_items << [item_1, item_2, item_3]
-  #
-  #     expect(user.noped_items).to eq([item_1, item_2, item_3])
-  #   end
-  #
-  # end
+
+  describe "#nopes" do
+
+    it "returns a list of nopes" do
+      item = Item.create(
+        name: "SAGSTUA",
+        image_url: "https://www.ikea.com/us/en/images/products/sagstua-bed-frame__0783215_PE761511_S5.JPG?f=s",
+        price: "149.00",
+        description: "Bed frame, black, Full",
+        more_info_url: "https://www.ikea.com/us/en/p/sagstua-bed-frame-black-s59268898/"
+      )
+      nope_1 = Nope.create(user_id: 1, item_id: item.id)
+      nope_2 = Nope.create(user_id: 2, item_id: item.id)
+      item.nopes << [nope_1, nope_2]
+
+      expect(item.nopes).to eq([nope_1, nope_2])
+    end
+
+  end
+
+  describe "#noping_users" do
+
+    it "returns a list of noping users" do
+      item = Item.create(
+        name: "SAGSTUA",
+        image_url: "https://www.ikea.com/us/en/images/products/sagstua-bed-frame__0783215_PE761511_S5.JPG?f=s",
+        price: "149.00",
+        description: "Bed frame, black, Full",
+        more_info_url: "https://www.ikea.com/us/en/p/sagstua-bed-frame-black-s59268898/"
+      )
+      user_1 = User.create(email: "email1@email.com", password: "password")
+      user_2 = User.create(email: "email2@email.com", password: "password")
+      user_3 = User.create(email: "email3@email.com", password: "password")
+      item.noping_users << [user_1, user_2, user_3]
+
+      expect(item.noping_users).to eq([user_1, user_2, user_3])
+    end
+
+  end
 
 end
