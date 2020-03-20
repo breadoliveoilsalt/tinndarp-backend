1a39db3e4c6b4bb20b9dcfaed3acea87e2aeac42
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-20 13:24:25 -0400

    Add User failing test for find_liked_items_in_common_with

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,32 @@ RSpec.describe User, type: :model do
   valid_email = "tony@tony.com"
   valid_password = "tonytony"
 
+  before(:each) do
+    @item_1 = Item.create(
+      name: "SAGSTUA",
+      image_url: "https://www.ikea.com/us/en/images/products/sagstua-bed-frame__0783215_PE761511_S5.JPG?f=s",
+      price: "149.00",
+      description: "Bed frame, black, Full",
+      more_info_url: "https://www.ikea.com/us/en/p/sagstua-bed-frame-black-s59268898/"
+    )
+
+    @item_2 = Item.create(
+      name: "MALM",
+      image_url: "https://www.ikea.com/us/en/images/products/malm-bed-frame-high__0637598_PE698416_S5.JPG?f=s",
+      price: "199.00",
+      description: "Bed frame, high, black-brown, Luröy, Queen",
+      more_info_url: "https://www.ikea.com/us/en/p/malm-bed-frame-high-black-brown-luroey-s69009475/"
+    )
+
+    @item_3 = Item.create(
+      name: "BUSKBO",
+      image_url: "https://www.ikea.com/us/en/images/products/buskbo-armchair__0700959_PE723853_S5.JPG?f=s",
+      price: "130.00",
+      description: "Armchair, rattan",
+      more_info_url: "https://www.ikea.com/us/en/p/buskbo-armchair-rattan-70434311/"
+    )
+  end
+
   describe "creating a User" do
 
     it "is not valid without an email" do
@@ -76,34 +102,10 @@ RSpec.describe User, type: :model do
   describe "#liked_items" do
 
     it "returns a list of liked items" do
-      item_1 = Item.create(
-        name: "SAGSTUA",
-        image_url: "https://www.ikea.com/us/en/images/products/sagstua-bed-frame__0783215_PE761511_S5.JPG?f=s",
-        price: "149.00",
-        description: "Bed frame, black, Full",
-        more_info_url: "https://www.ikea.com/us/en/p/sagstua-bed-frame-black-s59268898/"
-      )
-
-      item_2 = Item.create(
-        name: "MALM",
-        image_url: "https://www.ikea.com/us/en/images/products/malm-bed-frame-high__0637598_PE698416_S5.JPG?f=s",
-        price: "199.00",
-        description: "Bed frame, high, black-brown, Luröy, Queen",
-        more_info_url: "https://www.ikea.com/us/en/p/malm-bed-frame-high-black-brown-luroey-s69009475/"
-      )
-
-      item_3 = Item.create(
-        name: "BUSKBO",
-        image_url: "https://www.ikea.com/us/en/images/products/buskbo-armchair__0700959_PE723853_S5.JPG?f=s",
-        price: "130.00",
-        description: "Armchair, rattan",
-        more_info_url: "https://www.ikea.com/us/en/p/buskbo-armchair-rattan-70434311/"
-      )
-
       user = User.create(email: valid_email, password: valid_password)
-      user.liked_items << [item_1, item_2, item_3]
+      user.liked_items << [@item_1, @item_2, @item_3]
 
-      expect(user.liked_items).to eq([item_1, item_2, item_3])
+      expect(user.liked_items).to eq([@item_1, @item_2, @item_3])
     end
 
   end
@@ -124,43 +126,26 @@ RSpec.describe User, type: :model do
   describe "#noped_items" do
 
     it "returns a list of noped items" do
-      item_1 = Item.create(
-        name: "SAGSTUA",
-        image_url: "https://www.ikea.com/us/en/images/products/sagstua-bed-frame__0783215_PE761511_S5.JPG?f=s",
-        price: "149.00",
-        description: "Bed frame, black, Full",
-        more_info_url: "https://www.ikea.com/us/en/p/sagstua-bed-frame-black-s59268898/"
-      )
-
-      item_2 = Item.create(
-        name: "MALM",
-        image_url: "https://www.ikea.com/us/en/images/products/malm-bed-frame-high__0637598_PE698416_S5.JPG?f=s",
-        price: "199.00",
-        description: "Bed frame, high, black-brown, Luröy, Queen",
-        more_info_url: "https://www.ikea.com/us/en/p/malm-bed-frame-high-black-brown-luroey-s69009475/"
-      )
-
-      item_3 = Item.create(
-        name: "BUSKBO",
-        image_url: "https://www.ikea.com/us/en/images/products/buskbo-armchair__0700959_PE723853_S5.JPG?f=s",
-        price: "130.00",
-        description: "Armchair, rattan",
-        more_info_url: "https://www.ikea.com/us/en/p/buskbo-armchair-rattan-70434311/"
-      )
-
       user = User.create(email: valid_email, password: valid_password)
-      user.noped_items << [item_1, item_2, item_3]
+      user.noped_items << [@item_1, @item_2, @item_3]
 
-      expect(user.noped_items).to eq([item_1, item_2, item_3])
+      expect(user.noped_items).to eq([@item_1, @item_2, @item_3])
     end
 
   end
 
-  # describe "#find_liked_items_in_common_with" do
+  describe "#find_liked_items_in_common_with" do
 
-  #   it "returns an array of items that the user has in common with another user" do
+    it "returns an array of items that the user has in common with another user" do
+      user = User.create(email: "billy@billy.com", password: "password")
+      user.liked_items << [@item_1, @item_2]
+      compared_user = User.create(email: "johnny@johnny.com", password: "password")
+      compared_user.liked_items << [@item_1, @item_2]
 
-  #   end
+      result = user.find_liked_items_in_common_with(compared_user)
+
+      expect(result).to eq([@item_1, @item_2])
+    end
 
   #   it "returns an empty array if the user has no items in common with another user" do
 
@@ -170,6 +155,6 @@ RSpec.describe User, type: :model do
 
   #   end
 
-  # end
+  end
 
 end
